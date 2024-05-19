407ca9e0303cbec55711af2ebb99c43a266f7942
kro5s <111_111_1110@bk.ru>
2024-05-19 14:11:45 +0500

    frontend commit

--- nodejsupermacy/src/Service/Task.ts
+++ nodejsupermacy/src/Service/Task.ts
@@ -1,17 +1,17 @@
 import App from "../App";
 
 async function get() {
-	const app = App.get()
-	app.prisma.task.findMany({
-		where: {
-			starts: {
-				lte: new Date()
-			},
-			outdates: {
-				gte: new Date()
-			}
-		}
-	})
+	// const app = App.get()
+	// app.prisma.task.findMany({
+	// 	where: {
+	// 		starts: {
+	// 			lte: new Date()
+	// 		},
+	// 		outdates: {
+	// 			gte: new Date()
+	// 		}
+	// 	}
+	// })
 }
 
 export default {}
